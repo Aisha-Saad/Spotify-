f7a290df4ac8c10b61d00112bcdbcfd351267f7f
Aisha <111194434+Aisha-Saad@users.noreply.github.com>
2025-04-21 10:50:06 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import Sidebar from "./components/Sidebar";
 import Player from "./components/Player";
 import Display from "./components/Display";
+import { PlayerContext } from "./context/PlayerContext";
 
 const App = () => {
+
+  const {audioRef,track}=useContext(PlayerContext)
   return(
 
     <div className=" h-screen bg-black">
@@ -12,9 +15,10 @@ const App = () => {
         <Sidebar/>
         <Display/>
       
+      
         </div> 
         <Player/>
-
+        <audio ref={audioRef} src={track.file} preload="auto"></audio>
     </div>
 
   ) 
